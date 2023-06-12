91615326891f3008b2ead9457cc195bb33ee6c0a
xile611 <dingling112@gmail.com>
2023-06-12 15:51:16 +0800

    chore: add pre-release and release script

---
+++ common/scripts/pre-release.js
@@ -0,0 +1,49 @@
+/**
+ * prelease 
+ */
+
+const { spawnSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+function getPackageJson(pkgJsonPath) {
+  const pkgJson = fs.readFileSync(pkgJsonPath, { encoding: 'utf-8' })
+  return JSON.parse(pkgJson);
+}
+
+
+function run() {
+  const preReleaseName = process.argv.slice(2)[0] || 'alpha'
+  const cwd = process.cwd();
+
+  if (typeof preReleaseName === 'string' && preReleaseName) {
+    const preReleaseType = preReleaseName.includes('.') ? preReleaseName.split('.')[0] : 'alpha'; 
+
+    spawnSync('sh', ['-c', `rush publish --apply --prerelease-name ${preReleaseName} --partial-prerelease`], {
+      stdio: 'inherit',
+      shell: false,
+    });
+
+    spawnSync('sh', ['-c', `rush publish --publish --include-all --tag ${preReleaseType}`], {
+      stdio: 'inherit',
+      shell: false,
+    });
+
+    const rushJson = getPackageJson(`${cwd}/rush.json`)
+    const package = rushJson.projects.find((project) => project.packageName === '@visactor/vgrammar');
+    
+    if (package) {
+      const pkgJsonPath = path.resolve(package.projectFolder, 'package.json')
+      const pkgJson = getPackageJson(pkgJsonPath)
+
+
+      spawnSync('sh', ['-c', `git commit -m "build: prerelease version ${pkgJson.version}"`], {
+        stdio: 'inherit',
+        shell: false,
+      });
+    }
+  }
+}
+
+run()
+
---
+++ common/scripts/release.js
@@ -0,0 +1,49 @@
+/**
+ * prelease 
+ */
+
+const { spawnSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+function getPackageJson(pkgJsonPath) {
+  const pkgJson = fs.readFileSync(pkgJsonPath, { encoding: 'utf-8' })
+  return JSON.parse(pkgJson)
+}
+
+
+function run() {
+  const preReleaseName = process.argv.slice(2)[0] || 'alpha'
+  const cwd = process.cwd();
+
+  spawnSync('sh', ['-c', `rush version --bump`], {
+    stdio: 'inherit',
+    shell: false,
+  });
+
+  spawnSync('sh', ['-c', `rush update`], {
+    stdio: 'inherit',
+    shell: false,
+  });
+
+  const rushJson = getPackageJson(`${cwd}/rush.json`);
+  const package = rushJson.projects.find((project) => project.name === '@visactor/vgrammar');
+
+  if (package) {
+    const pkgJsonPath = path.resolve(project.projectFolder, 'package.json')
+    const pkgJson = getPackageJson(pkgJsonPath)
+
+    spawnSync('sh', ['-c', `git tag v${pkgJson.versopn}`], {
+      stdio: 'inherit',
+      shell: false,
+    });
+
+    spawnSync('sh', ['-c', 'rush publish --publish --include-all'], {
+      stdio: 'inherit',
+      shell: false,
+    });
+  }
+}
+
+run()
+
