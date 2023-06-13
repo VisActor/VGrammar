7470fed931d994e315c8787db715053735fb11e8
xile611 <dingling112@gmail.com>
2023-06-13 23:09:07 +0800

    chore: update pre-release to auto generate per-release name

--- common/scripts/pre-release.js
+++ common/scripts/pre-release.js
@@ -11,14 +11,48 @@ function getPackageJson(pkgJsonPath) {
   return JSON.parse(pkgJson);
 }
 
+const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-(alpha|beta|rc)(?:\.(?:(0|[1-9])))*)$/;
+
+const preReleaseNameReg = /^((alpha|beta|rc)(?:\.(?:0|[1-9]))*)$/;
+
 
 function run() {
-  const preReleaseName = process.argv.slice(2)[0] || 'alpha'
+  let preReleaseName = process.argv.slice(2)[0];
+  let preReleaseType = '';
   const cwd = process.cwd();
+  const rushJson = getPackageJson(`${cwd}/rush.json`)
+  const package = rushJson.projects.find((project) => project.packageName === '@visactor/vgrammar');
+  let regRes = null;
 
-  if (typeof preReleaseName === 'string' && preReleaseName) {
-    const preReleaseType = preReleaseName.includes('.') ? preReleaseName.split('.')[0] : 'alpha'; 
 
+  if (typeof preReleaseName === 'string' && preReleaseName && (regRes = preReleaseNameReg.exec(preReleaseName))) {
+    preReleaseType = regRes[2]; 
+  } else if (!preReleaseName) {
+    if (package) {
+      const pkgJsonPath = path.resolve(package.projectFolder, 'package.json')
+      const pkgJson = getPackageJson(pkgJsonPath)
+      const currentVersion = pkgJson.version;
+
+      if ((regRes = semverRegex.exec(currentVersion))) {
+        preReleaseType = regRes[4];
+
+        if (regRes[5]) {
+          preReleaseName = `${preReleaseType}.${parseInt(regRes[5], 10) + 1}`;
+        } else {
+          preReleaseName = `${preReleaseType}.0`;
+        }
+
+        console.log(`\x1b[31m[warning]\x1b[0m no prerelease-name supply, auto calculate prerelease-name \x1b[31m${preReleaseName}\x1b[0m`);
+      } else {
+        preReleaseName = `alpha.0`;
+        preReleaseType = 'alpha'; 
+
+        console.log('\x1b[31m[warning]\x1b[0m no prerelease-name supply, default to \x1b[31m alpha.0\x1b[0m')
+      }
+    }
+  }
+
+  if (preReleaseName && preReleaseType) {
     // 1. build all the packages
     spawnSync('sh', ['-c', `rush build --only tag:package`], {
       stdio: 'inherit',
@@ -42,9 +76,6 @@ function run() {
       stdio: 'inherit',
       shell: false,
     });
-
-    const rushJson = getPackageJson(`${cwd}/rush.json`)
-    const package = rushJson.projects.find((project) => project.packageName === '@visactor/vgrammar');
     
     if (package) {
       const pkgJsonPath = path.resolve(package.projectFolder, 'package.json')
