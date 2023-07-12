b66f57d4ecbe62fc422fd268e68bd6bb2e4867b4
xile611 <dingling112@gmail.com>
2023-07-12 16:15:42 +0800

    test: add test case of line element

--- packages/vgrammar/__tests__/mark/line.test.ts
+++ packages/vgrammar/__tests__/mark/line.test.ts
@@ -1,9 +1,10 @@
 import { createSimpleElement } from '../util';
 
-test('cornerRadius can be set to Rect', function () {
+test('addState to line', function () {
   const encode = {
     enter: { x: 1, y: 2 },
-    selected: { fillOpacity: 0.5 }
+    selected: { fillOpacity: 0.5 },
+    hover: { x: 3 }
   };
   const element = createSimpleElement('line', {
     markSpec: {
@@ -28,5 +29,16 @@ test('cornerRadius can be set to Rect', function () {
   element.addState('selected');
   expect(element.getStates()).toEqual(['selected']);
   expect(element.getGraphicItem().states).toEqual({ selected: encode.selected });
+  expect(element.getGraphicAttribute('fillOpacity')).toBe(0.5);
+
+  element.addState('hover');
+  expect(element.getStates()).toEqual(['selected', 'hover']);
+  expect(element.getGraphicItem().states.hover).toEqual({
+    points: [
+      { x: 3, y: 2, context: 0 },
+      { x: 3, y: 2, context: 1 }
+    ],
+    segments: null
+  });
   expect(element.getGraphicAttribute('fillOpacity')).toBe(0.5);
 });
