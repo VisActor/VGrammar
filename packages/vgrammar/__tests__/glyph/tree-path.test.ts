3ef132886f71b07ab7a096cca0cf4b73d4b95aca
purpose233 <zhangsu.melon@gmail.com>
2023-08-01 19:55:39 +0800

    chore: add unit test cases for vgrammar

---
+++ packages/vgrammar/__tests__/glyph/tree-path.test.ts
@@ -0,0 +1,34 @@
+import { registerTreePathGlyph } from '../../src';
+import { getGlyph } from '../../src/view/register-glyph';
+import { createSimpleBoxplotElement } from '../util';
+
+registerTreePathGlyph();
+
+test('line path encode', function () {
+  const element = createSimpleBoxplotElement(getGlyph('treePath'));
+  element.updateData('key', [{ key: 0 }], 'key', {} as any);
+  element.initGraphicItem();
+  element.encodeItems(
+    element.items,
+    {
+      enter: {
+        direction: 'vertical',
+        startArrow: false,
+        endArrow: true,
+        // pathType: 'line',
+        x0: 10,
+        x1: 60,
+        y0: 60,
+        y1: 70,
+        thickness: 1,
+        lineWidth: 1
+      }
+    } as any,
+    {}
+  );
+  element.encodeGraphic();
+
+  expect(element.getGraphicAttribute('path', undefined, 'main')).toEqual('M10,60C10,65,60,65,60,70');
+  expect(element.getGraphicAttribute('path', undefined, 'startArrow')).toEqual('');
+  expect(element.getGraphicAttribute('path', undefined, 'endArrow')).toEqual('M57,67L60,70L63,67');
+});
---
+++ packages/vgrammar/__tests__/transforms/mark-overlap.test.ts
@@ -0,0 +1,17 @@
+import { transform } from '../../src/transforms/mark/mark-overlap';
+import { createSimpleElement } from '../util';
+
+test('mark overlap x', () => {
+  const elementA = createSimpleElement('symbol');
+  const elementB = createSimpleElement('symbol');
+  elementA.initGraphicItem();
+  elementB.initGraphicItem();
+  elementA.setGraphicAttributes({ x: 0, y: 0, size: 20 });
+  elementB.setGraphicAttributes({ x: 40, y: 0, size: 20 });
+
+  transform({ direction: 1, delta: 10 }, [elementA, elementB]);
+  expect(elementB.getGraphicAttribute('visible')).toBe(true);
+
+  transform({ direction: 1, delta: 60 }, [elementA, elementB]);
+  expect(elementB.getGraphicAttribute('visible')).toBe(false);
+});
