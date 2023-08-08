2e23b1aeba9a96371e74ab287c8b42c4c2d44047
xile611 <dingling112@gmail.com>
2023-08-08 17:11:03 +0800

    docs: add demos about hierarchy-data in plot

---
+++ docs/dev-demos/src/plot/circle-packing.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import { type IView, type IPlot, getPalette } from '@visactor/vgrammar';
+import data from '../data/hierarchy.json';
+import { registerCirclePackingTransforms } from '@visactor/vgrammar-hierarchy';
+
+registerCirclePackingTransforms();
+
+
+export const runner = (plot: IPlot) => {
+  // plot.coordinate('polar', { transpose: false })
+
+  plot.circlePacking()
+    .data(data, [{
+      type: 'circlePacking',
+      padding: [4],
+      gapWidth: [8, 4, 2],
+      maxDepth: 1
+    }])
+    .encode('color', 'name')
+    .scale('color', { range: getPalette() })
+    .label('name', { textStyle: { fill: '#333' }, position: 'center', overlap: false });
+};
+
+export const callback = (view: IView) => {
+  // do nothing
+};
