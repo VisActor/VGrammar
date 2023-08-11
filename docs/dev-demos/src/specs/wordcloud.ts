916c93fbc08dce3a95bd70af25d53bc6cb895e18
xile611 <dingling112@gmail.com>
2023-08-11 17:26:01 +0800

    fix(wordcloud): minFontSize should be greater than 0 in CloudLayout, dont try infinity, fix #117

--- docs/dev-demos/src/specs/wordcloud.ts
+++ docs/dev-demos/src/specs/wordcloud.ts
@@ -20,14 +20,83 @@ export const spec = {
   data: [
     {
       id: 'baseData',
-      values: baseData.slice(0, 30),
-      transform: [
+      values: [
         {
-          type: 'map',
-          as: 'weight',
-          callback: (datum: any) => {
-            return 300;
-          }
+          "name": "螺蛳粉",
+          "value": 957,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 0,
+          "__VCHART_DEFAULT_DATA_KEY": "_0",
+          "__VCHART_WORD_CLOUD_WEIGHT": 500,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 0
+        },
+        {
+          "name": "钵钵鸡",
+          "value": 942,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 1,
+          "__VCHART_DEFAULT_DATA_KEY": "_1",
+          "__VCHART_WORD_CLOUD_WEIGHT": 488.2506527415144,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 1
+        },
+        {
+          "name": "板栗",
+          "value": 842,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 2,
+          "__VCHART_DEFAULT_DATA_KEY": "_2",
+          "__VCHART_WORD_CLOUD_WEIGHT": 409.92167101827675,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 2
+        },
+        {
+          "name": "胡辣汤",
+          "value": 828,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 3,
+          "__VCHART_DEFAULT_DATA_KEY": "_3",
+          "__VCHART_WORD_CLOUD_WEIGHT": 398.95561357702354,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 3
+        },
+        {
+          "name": "关东煮",
+          "value": 665,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 4,
+          "__VCHART_DEFAULT_DATA_KEY": "_4",
+          "__VCHART_WORD_CLOUD_WEIGHT": 271.27937336814625,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 4
+        },
+        {
+          "name": "羊肉汤",
+          "value": 627,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 5,
+          "__VCHART_DEFAULT_DATA_KEY": "_5",
+          "__VCHART_WORD_CLOUD_WEIGHT": 241.51436031331593,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 5
+        },
+        {
+          "name": "热干面",
+          "value": 574,
+          "__VCHART_DEFAULT_DATA_SERIES_FIELD": "wordCloud_5",
+          "__VCHART_DEFAULT_DATA_INDEX": 6,
+          "__VCHART_DEFAULT_DATA_KEY": "_6",
+          "__VCHART_WORD_CLOUD_WEIGHT": 200,
+          "__VCHART_WORD_CLOUD_ANGLE": 0,
+          "__VCHART_WORD_CLOUD_FILLING_ANGLE": 0,
+          "VGRAMMAR_DATA_ID_KEY_16": 6
         }
       ]
     }
@@ -59,7 +128,7 @@ export const spec = {
       id: 'color',
       type: 'ordinal',
       range: ['#d5a928', '#652c90', '#939597'],
-      domain: { data: 'baseData', field: 'challenge_name' }
+      domain: { data: 'baseData', field: 'name' }
     }
   ],
   marks: [
@@ -69,35 +138,27 @@ export const spec = {
       transform: [
         {
           type: 'wordcloud',
-          size: [500, 500],
+          size: [170, 10],
           // "size": [{"signal": "width"}, {"signal": "height"}],
-          text: { field: 'challenge_name' },
-          fontFamily: 'HuaWenHeiTi',
-          fontSize: { field: 'sum_count' },
-          fontWeight: { field: 'weight' },
-          fontSizeRange: [10, 40], // 字体范围
-          padding: { signal: 'wordPadding' },
-          // rotate: { field: 'rotate' },
-          shrink: false, // ls是否支持缩小显示单词
-          enlarge: true, // 是否支持放大填满画布
-          minFontSize: 2, // 缩放时最小字号
-          clip: false,
-          // "spiral": "rectangular",
-          randomVisible: false,
-          shape: 'circle',
-          // layoutType: 'fast',
-          progressiveStep: 50,
-          rotate: () => {
-            return Math.random() > 0.5 ? 90 : 0;
-          }
+          text: { field: 'name' },
+          "padding": 1,
+          "spiral": "archimedean",
+          "shape": "circle",
+          "fontFamily": "sans-serif",
+          "fontStyle": "normal",
+          "shrink": true,
+          "clip": false,
+          "enlarge": true,
+          "minFontSize": 0,
+          "random": true
         }
       ],
       encode: {
         enter: {
-          text: { field: 'challenge_name' },
+          text: { field: 'name' },
           textAlign: 'center',
           baseline: 'alphabetic',
-          fill: { scale: 'color', field: 'challenge_name' },
+          fill: { scale: 'color', field: 'name' },
           // font: 'Helvetica Neue, Arial',
           fontFamily: { field: 'fontFamily' },
           fontWeight: { field: 'weight' }
