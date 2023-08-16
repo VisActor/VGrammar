ae6a16a2f1fa33c30bc93188628097bf84f9f789
xile611 <dingling112@gmail.com>
2023-08-16 19:19:46 +0800

    docs: update demos of plot.line

--- docs/site/vite.config.js
+++ docs/site/vite.config.js
@@ -1,15 +1,10 @@
-import pkg from '../../packages/vgrammar/package.json';
+import pkg from './package.json';
 import * as path from 'path';
 
 const { plugin: mdPlugin, Mode } = require('vite-plugin-markdown')
 
-const vGrammarPkgs = Object.keys(pkg.dependencies).filter(pkgName => pkgName.startsWith('@visactor/vgrammar-'));
+const vGrammarPkgs = Object.keys(pkg.devDependencies).filter(pkgName => pkgName.startsWith('@visactor/vgrammar-'));
 
-vGrammarPkgs.push('@visactor/vgrammar-projection');
-vGrammarPkgs.push('@visactor/vgrammar-wordcloud');
-vGrammarPkgs.push('@visactor/vgrammar-wordcloud-shape');
-vGrammarPkgs.push('@visactor/vgrammar-sankey');
-vGrammarPkgs.push('@visactor/vgrammar-hierarchy');
 
 export default {
   optimizeDeps: {
