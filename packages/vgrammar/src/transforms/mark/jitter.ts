37dc1a78539df98f76de847f7a06f97a783f483f
xile611 <dingling112@gmail.com>
2023-08-21 10:54:39 +0800

    refactor: optimize the implementation of jitter

--- packages/vgrammar/src/transforms/mark/jitter.ts
+++ packages/vgrammar/src/transforms/mark/jitter.ts
@@ -1,6 +1,58 @@
-import type { IElement, JitterTransformOptions } from '../../types';
-import { transform as jitterX } from './jitter-x';
-import { transform as jitterY } from './jitter-y';
+import type { IElement, JitterTransformOptions, JitterXTransformOptions, JitterYTransformOptions } from '../../types';
+import { getBandWidthOfScale } from '../../graph/mark/encode';
+import { isNil } from '@visactor/vutils';
+import { extent } from '@visactor/vgrammar-util';
+
+const jitterByChannel = (options: JitterTransformOptions, upstreamData: IElement[], channel: 'x' | 'y') => {
+  const mark = upstreamData[0].mark;
+  const scale = mark.getScalesByChannel()?.[channel];
+  const random = options.random ?? Math.random;
+  const ratio = Math.min((channel === 'x' ? options.widthRatio : options.heightRatio) ?? 0.4, 0.5);
+  const bandSize = (channel === 'x' ? options.width : options.height) ?? getBandWidthOfScale(scale);
+
+  if (isNil(bandSize)) {
+    let domain = extent(upstreamData, el => el.getItemAttribute(channel));
+
+    if (isNil(domain[0]) || isNil(domain[1]) || domain[0] === domain[1]) {
+      const viewBox = mark.view.getViewBox();
+      domain = channel === 'x' ? [viewBox.x1, viewBox.x2] : [viewBox.y1, viewBox.y2];
+    }
+    const length = upstreamData.length;
+
+    upstreamData.forEach((element, index) => {
+      element.setItemAttributes({ [channel]: domain[0] + (domain[1] - domain[0]) * random(index, length) });
+    });
+  } else {
+    const length = upstreamData.length;
+    upstreamData.forEach((element, index) => {
+      const val = element.getItemAttribute(channel);
+      const domain = [val - ratio * bandSize, val + ratio * bandSize];
+      element.setItemAttributes({ [channel]: domain[0] + (domain[1] - domain[0]) * random(index, length) });
+    });
+  }
+};
+
+/**
+ * 针对mark的dodge变换，支持x、y方向
+ */
+export const jitterY = (options: JitterYTransformOptions, upstreamData: IElement[]) => {
+  if (!upstreamData || upstreamData.length === 0 || !upstreamData[0]?.mark) {
+    return upstreamData;
+  }
+
+  return jitterByChannel(options, upstreamData, 'y');
+};
+
+/**
+ * 针对mark的dodge变换，支持x、y方向
+ */
+export const jitterX = (options: JitterXTransformOptions, upstreamData: IElement[]) => {
+  if (!upstreamData || upstreamData.length === 0 || !upstreamData[0]?.mark) {
+    return upstreamData;
+  }
+
+  return jitterByChannel(options, upstreamData, 'x');
+};
 
 /**
  * 针对mark的dodge变换，支持x、y方向
@@ -10,8 +62,8 @@ export const transform = (options: JitterTransformOptions, upstreamData: IElemen
     return upstreamData;
   }
 
-  jitterX(options, upstreamData);
-  jitterY(options, upstreamData);
+  jitterByChannel(options, upstreamData, 'x');
+  jitterByChannel(options, upstreamData, 'y');
 
   return upstreamData;
 };
