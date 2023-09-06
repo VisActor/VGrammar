e6702c65695009084b89af4f422697535a2501dd
xile611 <dingling112@gmail.com>
2023-09-06 15:27:30 +0800

    chore: add bundle analysis tasks

--- tools/bundler/src/global.d.ts
+++ tools/bundler/src/global.d.ts
@@ -2,3 +2,6 @@ declare module 'gulp-clean' {
   const m = (opt?: { force: boolean }): NodeJS.ReadWriteStream => {};
   export = m;
 }
+
+declare module 'rollup-plugin-sizes';
+declare module 'rollup-plugin-bundle-size';
