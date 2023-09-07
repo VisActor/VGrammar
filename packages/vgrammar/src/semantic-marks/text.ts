de1a5294d87631317beb770273073d851e449e96
xile611 <dingling112@gmail.com>
2023-09-07 18:54:00 +0800

    fix: fix `richJsx()` utils

--- packages/vgrammar/src/semantic-marks/text.ts
+++ packages/vgrammar/src/semantic-marks/text.ts
@@ -1,7 +1,8 @@
 import { GrammarMarkType } from '../graph/enums';
-import type { MarkSpec } from '../types';
+import type { IMark, MarkSpec } from '../types';
 import { Mark } from '../view/mark';
 import { createGraphicItem } from '../graph/util/graphic';
+import { transformsByType } from '../graph/attributes';
 
 export class Text extends Mark {
   declare markType: GrammarMarkType.text;
@@ -9,13 +10,22 @@ export class Text extends Mark {
 
   addGraphicItem(initAttrs: any, groupKey?: string) {
     const textConfig = initAttrs?.text;
+    const isRich = textConfig?.type === 'rich';
     const graphicItem = createGraphicItem(
-      this,
-      textConfig?.type === 'rich' ? GrammarMarkType.richtext : GrammarMarkType.text,
+      this as IMark,
+      isRich ? GrammarMarkType.richtext : GrammarMarkType.text,
       initAttrs
     );
 
+    if (isRich) {
+      initAttrs.textConfig = [];
+    }
+
     return super.addGraphicItem(initAttrs, groupKey, graphicItem);
+  }
+
+  getAttributeTransforms() {
+    return this.getGroupGraphicItem()?.type === 'richtext' ? transformsByType.richtext : transformsByType.text;
   }
 
   release(): void {
