192efe9d386b4addd74f2f35059ef5e199ac39f9
purpose <zhangsu.melon0@bytedance.com>
2023-09-26 17:51:14 +0800

    feat: support dependencies for interaction

--- packages/vgrammar-core/src/interactions/base.ts
+++ packages/vgrammar-core/src/interactions/base.ts
@@ -1,14 +1,38 @@
-import { isArray } from '@visactor/vutils';
-import type { IView, InteractionEventHandler } from '../types';
+import { array, isArray, isNil, isString, isValid } from '@visactor/vutils';
+import type { IBaseInteractionOptions, IGrammarBase, IView, InteractionEventHandler } from '../types';
 
-export abstract class BaseInteraction {
+export abstract class BaseInteraction<T extends IBaseInteractionOptions> {
   readonly view: IView;
 
-  constructor(view: IView) {
+  constructor(view: IView, options: T) {
     this.view = view;
+    this.depend(options?.dependencies);
   }
 
+  references: Map<IGrammarBase, number> = new Map();
+
   protected abstract getEvents(): Record<string, InteractionEventHandler | InteractionEventHandler[]>;
+
+  depend(grammar: IGrammarBase[] | IGrammarBase | string[] | string) {
+    this.references.clear();
+    array(grammar)
+      .map(grammar => (isString(grammar) ? this.view.getGrammarById(grammar) : grammar))
+      .filter(ref => !isNil(ref))
+      .forEach(ref => {
+        this.references.set(ref, (this.references.get(ref) ?? 0) + 1);
+      });
+  }
+
+  parameters() {
+    const params: any = {};
+    this.references.forEach((count, ref) => {
+      // upstream reference with no valid id will not be recorded in parameters
+      if (isValid(ref.id())) {
+        params[ref.id() as string] = ref.output();
+      }
+    });
+    return params;
+  }
 
   bind() {
     const events = this.getEvents();
