15b6983a9dde9389a5220760825010c5d8ce0401
xile611 <dingling112@gmail.com>
2023-10-19 16:54:23 +0800

    feat: support DataLabel in line/area mark

--- packages/vgrammar-core/src/types/theme.ts
+++ packages/vgrammar-core/src/types/theme.ts
@@ -14,6 +14,7 @@ import type {
   LineAxisAttributes,
   LineAxisGridAttributes,
   LineCrosshairAttrs,
+  LineDataLabelAttrs,
   LineLabelAttrs,
   PolygonCrosshairAttrs,
   RectCrosshairAttrs,
@@ -58,6 +59,7 @@ export interface IComponentTheme {
   arcLabel?: RecursivePartial<ArcLabelAttrs>;
   symbolLabel?: RecursivePartial<SymbolLabelAttrs>;
   pointLabel?: RecursivePartial<BaseLabelAttrs>;
+  lineDataLabel?: RecursivePartial<LineDataLabelAttrs>;
 
   datazoom?: RecursivePartial<DataZoomAttributes>;
 
