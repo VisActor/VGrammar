3185e9445d9c9561ba2ab8e102e96dc3c290a3c8
xile611 <dingling112@gmail.com>
2023-11-15 14:30:41 +0800

    Revert "fix: pase viewOptions to vrender stage"
    This reverts commit b1d4163f54e0d9d6d49aa491a940375f056a3892.

--- packages/vgrammar-core/src/graph/canvas-renderer.ts
+++ packages/vgrammar-core/src/graph/canvas-renderer.ts
@@ -197,12 +197,21 @@ export default class CanvasRenderer implements IRenderer {
     const stage =
       (viewOptions.stage as unknown as Stage) ??
       createStage({
-        ...viewOptions,
         width: this._width,
         height: this._height,
+        renderStyle: viewOptions.renderStyle,
+        viewBox: viewOptions.viewBox,
+        dpr: viewOptions.dpr,
+        canvas: viewOptions.renderCanvas,
+        canvasControled: viewOptions.canvasControled,
+        container: viewOptions.container,
         title: viewOptions.rendererTitle,
-        disableDirtyBounds: viewOptions.disableDirtyBounds ?? true,
-        autoRender: viewOptions.autoRender ?? true
+        beforeRender: viewOptions.beforeRender,
+        afterRender: viewOptions.afterRender,
+        disableDirtyBounds: true,
+        autoRender: true,
+        pluginList: viewOptions.pluginList,
+        enableHtmlAttribute: viewOptions.enableHtmlAttribute
       });
 
     if (viewOptions.options3d?.enable) {
