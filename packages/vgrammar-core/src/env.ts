e90110c59aa43a8815fec9f784485d8b38a4a3e6
xile611 <dingling112@gmail.com>
2023-11-22 14:22:36 +0800

    fix: remove transfrom of rect and fix env

--- packages/vgrammar-core/src/env.ts
+++ packages/vgrammar-core/src/env.ts
@@ -1,1 +1,12 @@
-export * from '@visactor/vrender-kits';
+import { initBrowserEnv, initNodeEnv } from '@visactor/vrender-kits';
+import { isBrowserEnv, isNodeEnv } from '@visactor/vrender-core';
+
+export const initAutoEnv = () => {
+  if (isBrowserEnv()) {
+    initBrowserEnv();
+  } else if (isNodeEnv()) {
+    initNodeEnv();
+  }
+};
+
+export { initBrowserEnv, initNodeEnv };
