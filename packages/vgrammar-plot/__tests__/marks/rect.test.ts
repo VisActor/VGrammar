9708745772a5acf0fafa56f2070667f08c2663d1
xile611 <dingling112@gmail.com>
2023-12-04 19:21:27 +0800

    test: fix test cases of vgrammar-plot

--- packages/vgrammar-plot/__tests__/marks/rect.test.ts
+++ packages/vgrammar-plot/__tests__/marks/rect.test.ts
@@ -1,8 +1,5 @@
 import '../util';
 import { Plot } from '../../src';
-import { initBrowserEnv } from '@visactor/vgrammar-core';
-
-initBrowserEnv();
 
 test('add rect by api and x has two fields', () => {
   const plot = new Plot({
