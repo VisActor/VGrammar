7884ef31f508a67b63114fedf49b9faf474fcac8
xile611 <dingling112@gmail.com>
2023-12-04 19:21:27 +0800

    test: fix test cases of vgrammar-plot

--- packages/vgrammar-plot/__tests__/marks/interval.test.ts
+++ packages/vgrammar-plot/__tests__/marks/interval.test.ts
@@ -1,8 +1,5 @@
 import '../util';
 import { Plot } from '../../src';
-import { initBrowserEnv } from '@visactor/vgrammar-core';
-
-initBrowserEnv();
 
 test('add interval by api', () => {
   const plot = new Plot({
