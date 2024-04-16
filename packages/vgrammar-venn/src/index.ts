ffc5ddf7ca948dbf9f920573d0d2e2c4fcf72a58
Howard Zhang <zamhown@live.com>
2024-04-16 15:39:14 +0800

    fix: export venn interface

--- packages/vgrammar-venn/src/index.ts
+++ packages/vgrammar-venn/src/index.ts
@@ -2,6 +2,8 @@ import { Factory } from '@visactor/vgrammar-core';
 
 import { transform, transformMark } from './venn';
 
+export * from './interface';
+
 export const registerVennTransforms = () => {
   Factory.registerTransform(
     'venn',
