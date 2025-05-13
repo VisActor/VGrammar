641a6c86d09efed404d8075d3281a443d5dae4e5
xile611 <dingling112@gmail.com>
2025-05-13 15:56:38 +0800

    fix: fix error when the type of stroke is array

--- packages/vgrammar-core/src/graph/attributes/line.ts
+++ packages/vgrammar-core/src/graph/attributes/line.ts
@@ -1,8 +1,7 @@
 import type { IColor, IColorStop, ISegment } from '@visactor/vrender-core';
 import type { IPointLike } from '@visactor/vutils';
-import { isNil, isString } from '@visactor/vutils';
-import { transformAttributes } from './transform';
-import { GrammarMarkType } from '../enums';
+// eslint-disable-next-line no-duplicate-imports
+import { isArray, isNil, isString } from '@visactor/vutils';
 import type { IElement } from '../../types';
 
 const isStopsEqual = (prev: IColorStop[], next: IColorStop[]) => {
@@ -40,6 +39,14 @@ const isColorAttrEqual = (prev: IColor, next: IColor) => {
     return false;
   }
 
+  if (isArray(prev)) {
+    if (prev.length !== (next as any).length) {
+      return false;
+    }
+
+    return prev.every((prevEntry, index) => isColorAttrEqual(prevEntry, (next as any)[index]));
+  }
+
   if (prev.gradient !== (next as any).gradient) {
     return false;
   }
@@ -53,10 +60,10 @@ const isColorAttrEqual = (prev: IColor, next: IColor) => {
 
   return prevKeys.every(key => {
     if (key === 'stops') {
-      return isStopsEqual(prev[key], next[key]);
+      return isStopsEqual(prev[key], (next as any)[key]);
     }
 
-    return prev[key] === next[key];
+    return (prev as any)[key] === (next as any)[key];
   });
 };
 
